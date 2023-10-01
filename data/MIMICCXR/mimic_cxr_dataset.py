675eb97deb2a223b68c50cd5f6d91cd15d4cf8d3
Simon Rankl <simon_rankl@mckinsey.com>
2023-10-01 11:29:22 +0200

    new comments in mimic_dataset script

--- data/MIMICCXR/mimic_cxr_dataset.py
+++ data/MIMICCXR/mimic_cxr_dataset.py
@@ -10,19 +10,24 @@ from PIL import Image
 
 # %%
 class MIMICCXR(Dataset):
+   # The `__init__` method is the constructor of the `MIMICCXR` class. It initializes the object and
+   # sets its initial state. In this case, it takes in several arguments:
     def __init__(self, csv_labels, root_dir, classes, transform=None):
+        """
+        The function is a constructor for a dataset class representing the MIMICCXR dataset, taking in
+        arguments for csv_labels, root_dir, classes, and an optional transform.
         
+        :param csv_labels: A path to a csv file containing the labels. This csv file should have two
+        columns - one for the image file names and another for the corresponding labels
+        :param root_dir: The root directory where the image files are stored. This is the path to the
+        directory on the server where the images are located
+        :param classes: The "classes" parameter is a list that contains the names of the different
+        classes or categories in the dataset. 
+        :param transform: The transform parameter is an optional argument that allows you to apply
+        transformations to the sample images. Transformations can include resizing, cropping, rotating,
+        flipping, normalizing, etc. 
         """
-            Dataset class representing MIMICCXR dataset
-            
-            Arguments:
-            csv_labels: A path to a csv file containing the labels.
-            root_dir: Path to the image directory on the server
-            transform: Optional if we want to transform the sample images
-            
-            Returns: image data and label
-            
-        """
+        
         self.img_labels = csv_labels 
         self.img_dir = root_dir
         self.transform = transform
@@ -34,6 +39,16 @@ class MIMICCXR(Dataset):
         return len(self.img_labels)
     
     def __getitem__(self, idx):
+        """
+        The `__getitem__` function retrieves an image and its corresponding label from a dataset, given
+        an index.
+        
+        :param idx: The `idx` parameter is the index of the item that you want to retrieve from the
+        dataset. It is used to access the corresponding image and label data
+        :return: The `__getitem__` method returns the image and label corresponding to the given index.
+        The image is returned as a PIL image object, and the label is returned as a torch.FloatTensor
+        object.
+        """
         if torch.is_tensor(idx):
                     idx = idx.tolist()
         
